9430d0ac4499f26a83652e3722e36e593434c48c
vaishnaviK06 <kotawarvaishnavi@gmail.com>
2021-02-25 16:21:13 -0800

    React Setup

---
+++ backpackersApp/frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Test Code</h1>
+    );
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
